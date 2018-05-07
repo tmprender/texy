25f2882625156e4cc39ca3975e4421de8caf2ead
Stephen Groat <stephen.e.groat@gmail.com>
2018-05-07 04:34:51 -0400

    simplification try

--- semant.ml
+++ semant.ml
@@ -191,10 +191,9 @@ let check program =
       | Id s       -> (type_of_identifier s, SId s)
       | Assign(var, e) as ex -> 
           let lt = expr var
-          and rt = expr e in
-          let err = "illegal assignment " ^ string_of_expr lt ^ " = " ^ 
-            string_of_typ rt ^ " in " ^ string_of_expr ex
-          in (check_assign lt rt err, SAssign(lt, rt))
+          and rt = expr e 
+          in (check_assign lt rt ("illegal assignment " ^ string_of_expr lt ^ " = " ^ 
+            string_of_typ rt ^ " in " ^ string_of_expr ex), SAssign(lt, rt))
       | Unop(op, e) as ex -> 
           let (t, e') = expr e in
           let ty = match op with
