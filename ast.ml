4f7127608d96803a375ad0e520f92c478048cd92
Stephen Groat <stephen.e.groat@gmail.com>
2018-05-07 03:58:23 -0400

    Printing function change

--- ast.ml
+++ ast.ml
@@ -89,7 +89,7 @@ let rec string_of_expr = function
   | Binop(e1, o, e2) ->
       string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
   | Unop(o, e) -> string_of_uop o ^ string_of_expr e
-  | Assign(v, e) -> v ^ " = " ^ string_of_expr e
+  | Assign(v, e) -> string_of_expr v ^ " = " ^ string_of_expr e
   | Call(f, el) ->
       f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
   | ArrAcc(n, e) ->
